6816fc696a4760525e4d2cea61a2a5665ad6f8f9
rubiyadav18 <73836805+rubiyadav18@users.noreply.github.com>
2020-11-24 23:25:46 -0800

    Add files via upload

---
+++ ques_3.py
@@ -0,0 +1,15 @@
+# isEven()
+
+# def isEven():
+#     if(12%2==0):
+#         print("Even Number")
+#     else:
+#         print("Old Number")
+# Right code
+def isEven():
+    if(12%2==0):
+        print("Even Number")
+    else:
+        print("Old Number")
+isEven()
+
