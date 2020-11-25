cb6dd78880c28861054b09f2d5135ce3d3daa5a4
rubiyadav18 <73836805+rubiyadav18@users.noreply.github.com>
2020-11-24 23:27:03 -0800

    Add files via upload

--- ques_5.py
+++ ques_5.py
@@ -1,16 +1,7 @@
-def say_hello_language(name, language):
-    if language == "hindi":
-        print ("Namaste ", name)
-        print ("Aap kaise ho?")
-    elif language == "punjabi":
-        print ("Sat sri akaal ", name)
-        print ("Tuhada ki haal hai?")
-    else:
-        print ("Hello ", name)
-        print ("How are you?")
-say_hello_language("Rishabh", "punjabi")
-say_hello_language("Armaan", "english")
-say_hello_language("Abhishek", "french")
-
-
-
+def add_numbers(number1, number2):
+    print ("Main do numbers ko add karunga.")
+    print (number1 + number2)
+add_numbers(120, 50)
+num_x = 134
+num1_y = 45
+add_numbers(num_x,num1_y)
