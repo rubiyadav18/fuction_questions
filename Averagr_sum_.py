ee902a412ec72e053c77c03411e5c5b65e36414b
rubiyadav18 <73836805+rubiyadav18@users.noreply.github.com>
2020-12-01 19:53:37 -0800

    Add files via upload

---
+++ Averagr_sum_.py
@@ -0,0 +1,9 @@
+def averge():
+    sum=a+b+c
+    print(sum)
+    averge=sum//3
+    print(averge)
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=int(input("enter a number"))
+averge()
