95e23528e00354c19d88531c986caad2df5a416e
rubiyadav18 <73836805+rubiyadav18@users.noreply.github.com>
2021-01-01 00:56:38 +0530

    Add files via upload

--- question_1.py
+++ question_1.py
@@ -1,4 +1,11 @@
-def fuction():
-    print("who is the founder of fecabook")
-    print("mark zucherbergy")
-fuction()
+def add():
+    i=1
+    while i<=100:
+        if i%3==0:
+            print("nav")
+        if i%7==0:
+            print("gurukul")
+        if i%21==0:
+            print("navgurukul")
+        i=i+1
+add()
